c87cb442b351f70eb18f683b0adf601fb4edeeb8
clément4444 <clement.fiquet0@gmail.com>
2024-12-03 18:12:26 +0100

    anti conflit

--- client/src/components/form/Form.jsx
+++ client/src/components/form/Form.jsx
@@ -1,23 +1,9 @@
-import React, { useState } from 'react';
-import style from './form.module.css';
+import style from "./form.module.css"
 
-const Form = ({ menuList }) => {
-    const [propositions, setPropositions] = useState([]);
-
-    const generatePropositions = () => {
-        // Logique pour générer des propositions aléatoires à partir de menuList
-        const randomPropositions = menuList.sort(() => 0.5 - Math.random()).slice(0, 3);
-        setPropositions(randomPropositions);
-    };
-
+const Form = () => {
     return (
-        <div className={style.formContainer}>
-            <button onClick={generatePropositions}>Générer des propositions</button>
-            <ul>
-                {propositions.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+        <div>
+
         </div>
     );
 };
--- client/src/components/shop/Shop.jsx
+++ client/src/components/shop/Shop.jsx
@@ -1,19 +1,14 @@
-import React from 'react';
-import style from './shop.module.css';
+import style from "./shop.module.css";
+import MenuList from "../menuList/menuList";
+import Article from "../article/Article";
 
 const Shop = () => {
     return (
-        <div className={style.shopContainer}>
-            <div className={style.menuList}>
-                {/* le code pour le menuList */}
-                <input type="text" placeholder="Rechercher des soins..." />
-            </div>
-            <div className={style.articles}>
-                <div className={style.article}>Article 1</div>
-                <div className={style.article}>Article 2</div>
-                <div className={style.article}>Article 3</div>
-                <div className={style.article}>Article 4</div>
-            </div>
+        <div>
+            <MenuList />
+            <Article />
+            <Article />
+            <Article />
         </div>
     );
 };
