2db6f8e13a240cbaf7e621942aa51b1e25db19ea
Philippevde <contact@van-der-eecken.eu>
2024-12-04 10:42:29 +0100

    banner

--- client/src/pages/detailsPage/DetailsPage.jsx
+++ client/src/pages/detailsPage/DetailsPage.jsx
@@ -1,14 +1,22 @@
-import style from "./detailsPage.module.css";
+import React from 'react';
+import styles from "./detailsPage.module.css";
 import Recipe from "../../components/recipe/Recipe";
 import Form from "../../components/form/Form";
 
 const DetailsPage = () => {
-	return (
-		<div>
-			<Recipe />
-			<Form />
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <div className={styles.banner}>
+                <img src="/images/image_2.png" alt="Banner" className={styles.bannerImage} />
+                <div className={styles.overlay}>
+                    <h1 className={styles.bannerTitle}>Votre recette</h1>
+                    <h2 className={styles.bannerSubTitle}></h2>
+                </div>
+            </div>
+            <Recipe />
+            <Form />
+        </div>
+    );
 };
 
 export default DetailsPage;
